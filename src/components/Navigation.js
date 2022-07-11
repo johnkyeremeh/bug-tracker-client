0f96e1eba28fb6cefcb314c90e0188f59c9bac15
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-10 17:21:32 -0700

    Add constructor to connect to props. Imported Connect from react-redux and passed dispatch from MapStateToProps that gave us dispatch as a prop to Nav. Connected container with Store State with mapStateToProps that gave us CurrentUser

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,24 +2,34 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import {connect} from "react-redux"
-// import Logout from './Logout';
 
 import { clearCurrentUser } from '../actions/currentUser';
+import {connect} from "react-redux"
+
+
+import { useHistory } from 'react-router-dom';
+
+
+
+// import Logout from './Logout';
+
 
 import "../Navigation.css"
 
-function Navigation() {
+function Navigation(props) {
   
+  const history = useHistory();
+
   //log user out
   const LogoutUser = (event) => {
+    console.log("log out clicked")
     event.preventDefault()
+    props.dispatch(clearCurrentUser())
+    
     localStorage.removeItem("token")
 
-    return dispatch => {
-        dispatch(clearCurrentUser)
-        console.log("logging user out")
-    }
+    // window.location.reload(true);
+    history.push("/login")
 }
 
   
@@ -29,18 +39,22 @@ function Navigation() {
         <Navbar.Brand href="/">BugTracker App</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
+          {props.currentUser? 
+          <>
+            <Navbar.Text>
+              Signed in as: <a href="#login">{props.currentUser.username}</a>
+            </Navbar.Text>
+            <Button onClick={LogoutUser} href="" variant="outline-light">Log Out</Button>
+          </> : 
+          <>
             <Nav.Link  className="linkText" href="./login">Sign in</Nav.Link>
             <Button href="/signup"variant="outline-light">Sign Up</Button>
             <Button onClick={LogoutUser} href="" variant="outline-light">Log Out</Button>
-         
-
-          {/* <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text> */}
+          </>}
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default connect(null, clearCurrentUser)(Navigation);
+export default connect(null, {clearCurrentUser})(Navigation);
