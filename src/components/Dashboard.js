fc81ccf790dd70363e3485aa1a875e382baef137
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-12 00:25:29 -0700

    Created HOME Component. Next Steps create only for logged_in users

---
+++ src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import {Component} from "react"
+
+
+class Dashboard extends Component{
+
+    render(){
+        return(
+            <div className="">
+            
+          </div>)
+    }
+
+
+
+}
+
+export default MainComponent
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import NavigationContainer from "../containers/NavigationContainer";
+
+export default class HOME extends Component{
+
+    render(){
+        return(
+        <>
+           <NavigationContainer/>
+       
+        </>
+        )
+    }
+}
