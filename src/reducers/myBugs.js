a458cda029bcbe9a2177f3b4c1601ee0993a57a1
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-12 00:24:49 -0700

    Updated return of SET_MY_BUGS to match change in passed data

--- src/reducers/myBugs.js
+++ src/reducers/myBugs.js
@@ -1,9 +1,10 @@
 export default function myBugs(state = []
     , action) {
+
       console.log(action)
       switch(action.type) {
         case "SET_MY_BUGS":
-          return state
+          return state.concat(action.payload.data)
         case "CREATE_BUG":
             return [...state, action.payload]
         default:
