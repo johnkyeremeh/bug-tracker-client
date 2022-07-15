f1226f546e8139ce7b6441422bd7673c0ca8c15f
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-15 14:15:53 -0700

    Add ComponentDidMount lifecycle function to initiate getBug action creator. Connected state to Component and bug value. On load bug is empty

--- src/components/BugView.js
+++ src/components/BugView.js
@@ -1,23 +1,32 @@
 import { Component } from "react";
+import { updateBug, markComplete, deleteBug, getBug } from "../actions/myBugs";
+import { Link } from 'react-router-dom'
+import { connect } from "react-redux";
 
 class BugView extends Component{
 
+    componentDidMount() {                                   
+        this.props.getBug(this.props.match.params.id);
+    }
+
  
     render(){
-
-        console.log(this.props)
+        const bug = this.props.bug;
+       
         return(
             <div> 
-               <button></button>
-               Bug: id {this.props.bug.id}
-               Bug: Title: {this.props.bug.attributes.title}
-               Bug: Title: {this.props.bug.attributes.descrption}
-               Bug: Title: {this.props.bug.attributes.status}
-               Bug: Title: {this.props.bug.attributes.title}
+                {this.props.bug ? <><Link to={{ pathname: `/bugs/${bug.id}/edit`, state: { bug: bug } }} className='btn btn-info'>Edit Bug</Link> <button className="btn btn-danger" type="button" onClick={() => this.props.deleteBug(bug.id)}>Delete</button>
+                </> : "TST"}
             </div>    
         )
     }
 }
 
 
-  export default BugView
+const mapStateToProps = (state) => {
+    return { 
+        bug: state.bug 
+    }
+}           
+
+export default connect(mapStateToProps, {getBug} )(BugView); 
