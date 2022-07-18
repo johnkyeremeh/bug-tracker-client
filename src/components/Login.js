f4f9659294e52110c4ec8dcbced97e193046fb17
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-18 09:41:11 -0700

    Updated UX of login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,14 +23,40 @@ const Login = ({ loginFormData, updateLoginForm, login}) => {
     return(
         <div>
             <NavagitationContainer />
-            <form onSubmit={handleSubmit}>
-                <label>Username</label>
-                <input type="text" name="username" placeholder="username" onChange={handleChange} value={loginFormData.username} /><br/>
-                <label>Password</label>
-                <input type="password" name="password" placeholder="username" onChange={handleChange} value={loginFormData.password} /><br/>
-                <input type="submit" name="password"/>
-            </form>
-
+            <div className="Auth-form-container">
+                <form className="Auth-form" onSubmit={handleSubmit}>
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Sign In</h3>
+                            <div className="form-group mt-3">
+                                <label>Username</label>
+                                    <input
+                                    type="text"
+                                    name="username"
+                                    className="form-control mt-1"
+                                    placeholder="Enter your username"
+                                    onChange={handleChange} 
+                                    value={loginFormData.username}
+                                    />
+                            </div>
+                            <div className="form-group mt-3">
+                                <label>Password</label>
+                                <input
+                                type="password"
+                                name="password"
+                                className="form-control mt-1"
+                                placeholder="Enter password"
+                                onChange={handleChange} 
+                                value={loginFormData.password}
+                                />
+                            </div>
+                            <div className="d-grid gap-2 mt-3">
+                                <button type="submit" className="btn btn-primary">
+                                Submit
+                                </button>
+                            </div>
+                    </div>
+                </form>
+            </div>
         </div>)
    
 }
